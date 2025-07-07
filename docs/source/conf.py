d676fa9d2ada02a44ecc663d971b725d7b7e7347
tmaringgele <tmaringgele@outlook.at>
2025-07-07 13:16:43 +0200

    mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+autodoc_mock_imports = ["rpy2", "jpype1"]
+
 
 # Configuration file for the Sphinx documentation builder.
 #
