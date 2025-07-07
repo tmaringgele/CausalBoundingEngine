0d0bc5f7a760bfb5aebd55dfd55b426acc4a5f90
tmaringgele <tmaringgele@outlook.at>
2025-07-07 13:09:08 +0200

    Fix sys.path for ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
